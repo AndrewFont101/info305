47a7d407070d9629eb6277f32095c18ecc76d57e
fonan570 <ajfontein@gmail.com>
2020-03-12 15:59:39 +1300

    Lab 2

---
+++ app/src/main/java/com/example/milestone1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.milestone1';
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+   // private SensorManager mSensorManager;
+   // private Sensor mOrientation;
+
+    private ImageView imageView;
+
+    private float[] mGravity =new float[3];
+    private float[] nGeomagnetic = new float[3];
+
+}
